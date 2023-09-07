69e6df1785da6d461fac8ec8d00a9fc4881bd668
Prajjwaldabas <Prajjwalchaudhary29898@gmail.com>
2023-09-07 16:55:16 +0530

    third commit IQ section completed

---
+++ src/components/IQ_section/IQ_section.jsx
@@ -0,0 +1,20 @@
+
+import './IQ.css'
+const IQ_section = () => {
+  return (
+    <div className="w-100 flex IQ-container jcc ">
+
+<div className="EQ"><h2>EQ beats IQ</h2></div>
+        
+<div className="flex IQ-text">
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quia similique illum dolore recusandae est cupiditate aperiam officiis maxime  </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque aperiam dicta nemo odio nihil ea, consequuntur non eveniet corrupti dolorem ducimus</p>
+</div>
+
+
+
+    </div>
+  )
+}
+
+export default IQ_section
--- src/pages/main.jsx
+++ src/pages/main.jsx
@@ -1,11 +1,13 @@
 import Navbar from "../components/Navbar/Navbar"
 import Hero from '../components/Hero/Hero'
+import IQ_section from '../components/IQ_section/IQ_section'
 const Main = () => {
 
     return <div id="App">
 
         <Navbar/>
         <Hero/>
+        <IQ_section/>
        
         </div>
 
